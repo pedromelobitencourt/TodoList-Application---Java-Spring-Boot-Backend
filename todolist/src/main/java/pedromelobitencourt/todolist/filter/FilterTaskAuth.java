30a99b363e1c70b7eb37c4bba53412025a4c8e41
pedromelobitencourt <pedro.melopedromelo@outlook.com.br>
2023-10-16 18:55:17 -0300

    Modify task function

--- todolist/src/main/java/pedromelobitencourt/todolist/filter/FilterTaskAuth.java
+++ todolist/src/main/java/pedromelobitencourt/todolist/filter/FilterTaskAuth.java
@@ -27,7 +27,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
 
         var servletPath = request.getServletPath();
 
-        if(servletPath.equals("/tasks/")) {
+        if(servletPath.startsWith("/tasks/")) {
             // Get user and password
             var authorization = request.getHeader("Authorization");
             var authEncoded = authorization.substring("Basic".length()).trim();
--- todolist/src/main/java/pedromelobitencourt/todolist/task/TaskController.java
+++ todolist/src/main/java/pedromelobitencourt/todolist/task/TaskController.java
@@ -10,10 +10,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import pedromelobitencourt.todolist.utils.Utils;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -53,5 +57,15 @@ public class TaskController {
         var tasks = this.taskRepository.findByIdUser((UUID) idUser);
 
         return tasks;
+    }
+
+    @PutMapping("/{id}")
+    public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        Utils.copyNonNullProperties(taskModel, task);
+
+        return this.taskRepository.save(task);
     }
 }
